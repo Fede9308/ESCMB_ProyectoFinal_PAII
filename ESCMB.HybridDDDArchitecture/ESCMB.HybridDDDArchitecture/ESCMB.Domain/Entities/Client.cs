ff08d6d387277225927ed538fa412df96fa76173
Fede9308 <117412714+Fede9308@users.noreply.github.com>
2023-09-25 03:28:13 -0300

    Client entity created with validator still unfinished

--- ESCMB.HybridDDDArchitecture/ESCMB.HybridDDDArchitecture/ESCMB.Domain/Entities/Client.cs
+++ ESCMB.HybridDDDArchitecture/ESCMB.HybridDDDArchitecture/ESCMB.Domain/Entities/Client.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common.Domain.Entities;
+using ESCMB.Domain.Validators;
+using static ESCMB.Domain.Enums;
 
 namespace ESCMB.Domain.Entities
 {
-    internal class Client
+    internal class Client : DomainEntity<ClientValidator>
     {
+
+        public int Id { get; private set; } 
+
+        public string Nombre { get; private set; }
+
+        public string Apellido { get; private set; }    
+
+        public long DNI { get; private set; }
+
+        public string Email {get; private set; }
+
+
+
+
+
     }
 }
--- ESCMB.HybridDDDArchitecture/ESCMB.HybridDDDArchitecture/ESCMB.Domain/Validators/ClientValidator.cs
+++ ESCMB.HybridDDDArchitecture/ESCMB.HybridDDDArchitecture/ESCMB.Domain/Validators/ClientValidator.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common.Domain.Validators;
+using ESCMB.Domain.Entities;
+using FluentValidation;
+
 
 namespace ESCMB.Domain.Validators
 {
-    internal class ClientValidator
+    internal class ClientValidator : EntityValidator<Client>
     {
+        public ClientValidator() 
+        {
+            RuleFor(x => x.Nombre).NotNull().NotEmpty().WithMessage(Constants.NOTNULL_OR_EMPTY);
+            RuleFor(x => x.Apellido).NotNull().NotEmpty().WithMessage(Constants.NOTNULL_OR_EMPTY);
+            RuleFor(x => x.DNI).NotNull().NotEmpty().WithMessage(Constants.NOTNULL_OR_EMPTY);
+            
+        }
     }
 }
